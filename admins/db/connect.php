cec97865f22b86d86db5bf1bb451c127d947630e
Nguyễn Toại <150449878+tokomori@users.noreply.github.com>
2024-12-13 14:33:43 +0700

    Add files via upload

---
+++ admins/db/connect.php
@@ -0,0 +1,12 @@
+<?php
+$server = "localhost"; // IP hoặc domain kết nối để CSDL
+$username = "root"; // Người dùng sử dụng để kết nối đến CSDL với PHP
+$password = ""; // Mật khẩu sử dụng để kết nối đến CSDL với PHP
+$database = "shopgundam"; // Tên database
+
+// Kết nối đến MySQL với PHP sử dụng MySQLi
+$conn = new mysqli("localhost", "root", "", "shopgundam");
+
+
+
+?>
